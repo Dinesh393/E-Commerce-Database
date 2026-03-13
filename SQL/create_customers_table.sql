ca4e8f731479e8b81dcd25aa73814b6dd83f6f75
Dinesh <dineshpaloju453@gmail.com>
2026-03-13 20:19:46 +0530

    Created categories table

---
+++ SQL/create_customers_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE customers
+(
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(30) NOT NULL,
+    phone VARCHAR(20),
+    address VARCHAR(150),
+    created_at DATETIME
+);
