a674b7d365902e291fd42f84dfc652f66be7f2c1
Dinesh <dineshpaloju453@gmail.com>
2026-03-31 15:58:49 +0530

    Created orders table

---
+++ SQL/create_orders_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE orders
+(
+  order_id INT PRIMARY KEY,
+  customer_id INT,
+  order_date DATE,
+  order_status VARCHAR(50),
+  total_amount DECIMAL(20,2),
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+)
