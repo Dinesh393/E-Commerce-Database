bd24f74b98d36ccca338799ac14b846b619ff252
Dinesh <dineshpaloju453@gmail.com>
2026-03-31 16:11:55 +0530

    Created order_items Table

---
+++ SQL/create_order_items_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE order_items
+(
+  order_item_id INT PRIMARY KEY,
+  product_id INT,
+  order_id INT,
+  price DECIMAL(10,2),
+  quantity INT,
+  FOREIGN KEY (order_id) REFERENCES orders(order_id),
+  FOREIGN KEY(product_id) REFERENCES products(product_id)
+)
