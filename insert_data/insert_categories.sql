0616aeba6bcdb2f7b1549c03ef6e2c8f8230c7de
Dinesh <dineshpaloju453@gmail.com>
2026-04-03 17:34:50 +0530

    Inserted sample data into categories, customers and orders tables

---
+++ insert_data/insert_categories.sql
@@ -0,0 +1,11 @@
+-- CATEGORIES
+INSERT INTO categories (category_name) VALUES ('Electronics');
+INSERT INTO categories (category_name) VALUES ('Clothing');
+INSERT INTO categories (category_name) VALUES ('Home & Kitchen');
+INSERT INTO categories (category_name) VALUES ('Sports & Outdoors');
+INSERT INTO categories (category_name) VALUES ('Books');
+INSERT INTO categories (category_name) VALUES ('Beauty & Personal Care');
+INSERT INTO categories (category_name) VALUES ('Toys & Games');
+INSERT INTO categories (category_name) VALUES ('Automotive');
+INSERT INTO categories (category_name) VALUES ('Health & Wellness');
+INSERT INTO categories (category_name) VALUES ('Office Supplies');
